687b6626c8281fe0d90b5aa20cc58026e3e08eaf
Hill-0xFF <leadlevelprogrammer@gmail.com>
2023-06-22 13:34:59 -0300

    [dev]: Creating a custom layout component for about route

---
+++ chap02-pages-layouts/app/about/layout.tsx
@@ -0,0 +1,17 @@
+import './globals.css'
+
+const layout = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
+  return (
+    <div>
+      {
+        children
+      }
+    </div>
+  )
+}
+
+export default layout
