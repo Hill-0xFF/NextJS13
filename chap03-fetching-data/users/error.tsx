71ec00781b539f506490869355f92612cf521223
Hill-0xFF <leadlevelprogrammer@gmail.com>
2023-06-22 18:47:24 -0300

    [dev]: Chapter 3 - Added error.tsx, page.module.css, imported type Metadata and Link component

---
+++ chap03-fetching-data/users/error.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import type { Metadata } from "next"
+import Link from "next/link"
+
+import style from './page.module.css'
+
+const metadata: Metadata = {
+  title: 'USER PAGE ERROR',
+  description: 'Something went wrong on /users/',
+}
+
+export default function Error({
+  error,
+  reset
+}: {
+  error: Error,
+  reset: () => void,
+}) {
+
+  useEffect(() => {
+    console.error(error);
+    
+  },[error])
+  return (
+    <div className={style.main}>
+      <h2>Something went wrong on /users</h2>
+      <button onClick={() => reset()}></button>
+    </div>
+  )
+}
--- chap03-fetching-data/users/page.tsx
+++ chap03-fetching-data/users/page.tsx
@@ -1,17 +1,38 @@
-import type { Metadata } from "next";
-import getAllUsers from "@/lib/getAllUsers";
+import type { Metadata } from 'next';
+import Link from 'next/link';
+import getAllUsers from '@/lib/getAllUsers';
 
 export const metadata: Metadata = {
-  title: 'User Page'
-}
+  title: 'User Page',
+};
 
-export default function Users() {
+export default async function Users() {
   // this is a server component, so it's going to request it as it
   // builds the website/html (Static Generation), it won't request as a
   // request, it'll request at build time
-  // 
-  const usersData: Promise<IUsersData[]> = getAllUsers()
-  return (
-    <div>page</div>
-  )
+  //
+  const usersData: Promise<IUsersData[]> = getAllUsers();
+  const users = await usersData;
+  console.log('Hello this is a server component!!!');
+  
+
+  const content = (
+    <section>
+      <h2>
+        <Link href="/">Back Home</Link>
+      </h2>
+      <br />
+      {users.map((user) => {
+        return (
+          <>
+            <p key={user.id}>
+              <Link href={`/users/${user.id}`}>{user.name}</Link>
+            </p>
+            <br />
+          </>
+        );
+      })}
+    </section>
+  );
+  return content;
 }
