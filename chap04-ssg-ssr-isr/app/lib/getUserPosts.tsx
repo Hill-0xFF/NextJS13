e98b1f97a6cf3fa5ce7535eaaa8bb3dfa512ac7a
Hill-0xFF <leadlevelprogrammer@gmail.com>
2023-06-23 00:58:05 -0300

    [dev]: Chapter 4 - using ISR revalidate

--- chap04-ssg-ssr-isr/app/lib/getUserPosts.tsx
+++ chap04-ssg-ssr-isr/app/lib/getUserPosts.tsx
@@ -1,5 +1,5 @@
 export default async function getUserPosts(id: string) {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`, { next: {revalidate: 60}})
   if (!response.ok) throw new Error('Failed to fetch user!')
   return response.json()
 }
