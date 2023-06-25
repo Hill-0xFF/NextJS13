0ac7ad5c042b5ad4c8e44957d848345c0703da55
Hill-0xFF <leadlevelprogrammer@gmail.com>
2023-06-25 01:33:51 -0300

    [fix]: Chapter 5 - Need handle error: 'Unexpected token < in JSON at position 0' after submitting a search on the input field

--- chap05-projekt/lib/getWikipediaResults.tsx
+++ chap05-projekt/lib/getWikipediaResults.tsx
@@ -5,20 +5,20 @@
 export default async function getWikipediaResults(search: string) {
   const searchParams = new URLSearchParams({
     action: 'query',
-    generator: 'search',
-    gsrsearch: search,
-    gsrlimit: '20',
-    prop: 'pageimages|extracts',
-    exchars: '100',
-    exintro: 'true',
-    explaintext: 'true',
-    exlimit: 'max',
-    format: 'json',
-    origin: '*',
+        generator: 'search',
+        gsrsearch: search,
+        gsrlimit: '20',
+        prop: 'pageimages|extracts',
+        exchars: '100',
+        exintro: 'true',
+        explaintext: 'true',
+        exlimit: 'max',
+        format: 'json',
+        origin: '*',
   })
 
-  // const response = await fetch(`https://en.wikipedia.org/w/api.php?${search}`)
-  const response = await fetch('https://en.wikipedia.org/w/api.php?' + search)
+  const response = await fetch(`https://en.wikipedia.org/w/api.php?${search}`)
+  // const response = await fetch('https://en.wikipedia.org/w/api.php?' + search)
   if (!response.ok) throw new Error('Error while fetching search.')
   return response.json()
 }
